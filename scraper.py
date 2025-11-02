79ed7d1613c62183120745bdc6df3d14e040bad9
jshift0921 <jshift0921@gmail.com>
2025-11-02 22:04:55 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,8 +6,8 @@ def scrape_tsmc_stock_price():
     url = "https://s.shopee.tw/1LW8nu0as9"  # 台積電股價GOOGLE網站URL
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    stock_price = soup.find("div", class_="YMlKec fxKbKc").text  # 使用 BeautifulSoup 解析網頁並提取股價信息
-    return stock_price
+    Stock_quantity = soup.find("div", class_="YMlKec fxKbKc").text  # 使用 BeautifulSoup 解析網頁並提取股價信息
+    return Stock_quantity
 
 if __name__ == "__main__":
     stock_price = scrape_tsmc_stock_price()
