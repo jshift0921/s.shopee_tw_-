9113d0f9782c390aabc0d0e50ab3623585c8829d
jshift0921 <jshift0921@gmail.com>
2025-11-02 21:58:41 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,16 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-# 抓取蝦皮即時補貨
-def shopee_tw():
-    url = "https://s.shopee.tw/1LW8nu0as9"  # 蝦皮泡泡瑪特旗艦店
+# 抓取台積電股價
+def scrape_tsmc_stock_price():
+    url = "https://www.google.com/finance/quote/2330:TPE"  # 台積電股價GOOGLE網站URL
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    quantity = soup.find("div", class_="YMlKec fxKbKc").text  # 使用 BeautifulSoup 解析網頁並提取商品數量信息
-    return quantity
+    stock_price = soup.find("div", class_="YMlKec fxKbKc").text  # 使用 BeautifulSoup 解析網頁並提取股價信息
+    return stock_price
 
 if __name__ == "__main__":
-    Product_quantity = shopee_tw_back_in_stock()
-    message = f"不眠劇場毛絨掛件：{Product_quantity}"
+    stock_price = scrape_tsmc_stock_price()
+    message = f"台積電股價：{stock_price}"
     requests.post("https://ntfy.sh/JPopmart",
     data=message.encode(encoding='utf-8'))
