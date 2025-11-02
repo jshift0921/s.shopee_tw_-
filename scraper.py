1a691669cc6fbb961d908f883d0d9e3511783792
jshift0921 <jshift0921@gmail.com>
2025-11-02 22:01:49 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 # 抓取台積電股價
 def scrape_tsmc_stock_price():
-    url = "https://www.google.com/finance/quote/2330:TPE"  # 台積電股價GOOGLE網站URL
+    url = "https://s.shopee.tw/1LW8nu0as9"  # 台積電股價GOOGLE網站URL
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     stock_price = soup.find("div", class_="YMlKec fxKbKc").text  # 使用 BeautifulSoup 解析網頁並提取股價信息
