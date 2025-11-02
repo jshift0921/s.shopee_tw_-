5c85b4186e8b8aa3a7b0b2e48642e2c55bfa33b2
jshift0921 <jshift0921@gmail.com>
2025-11-02 21:30:47 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 # 抓取蝦皮即時補貨
 def shopee_tw_back_in_stock():
-    url = "https://s.shopee.tw/6pr5M2V17Y"  # 蝦皮泡泡瑪特旗艦店
+    url = "https://s.shopee.tw/1gA466VGXZ"  # 蝦皮泡泡瑪特旗艦店
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     quantity = soup.find("div", class_="YMlKec fxKbKc").text  # 使用 BeautifulSoup 解析網頁並提取商品數量信息
@@ -11,6 +11,6 @@ def shopee_tw_back_in_stock():
 
 if __name__ == "__main__":
     Product_quantity = shopee_tw_back_in_stock()
-    message = f"sp花生惡作劇/發霉小蛋糕耳機包：{Product_quantity}"
+    message = f"星期三：{Product_quantity}"
     requests.post("https://ntfy.sh/JPopmart",
     data=message.encode(encoding='utf-8'))
